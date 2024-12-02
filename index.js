1d14006a0b0c0c6a97797f711823cce15f558cd1
Marvin Calmo <calmomarvin93@gmail.com>
2024-12-02 16:15:02 +0100

    [fix] : security cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"] }));
 
 const sampleEmployee = [
 	{
